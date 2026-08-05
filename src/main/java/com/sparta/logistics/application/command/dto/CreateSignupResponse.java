ab80b3551aa91573e35f2acc1ef3bf50a419a24e
kanghm199 <kanghm199@gmail.com>
2026-08-05 14:26:32 +0900

    Feat: 회원가입 요청 기능 구현

---
+++ src/main/java/com/sparta/logistics/application/command/dto/CreateSignupResponse.java
@@ -0,0 +1,21 @@
+package com.sparta.logistics.application.command.dto;
+
+import com.sparta.logistics.domain.entity.AuthAccounts;
+import com.sparta.logistics.domain.model.AccountStatus;
+
+import java.util.UUID;
+
+public record CreateSignupResponse(
+    UUID userId,
+    String username,
+    AccountStatus status
+) {
+  public static CreateSignupResponse from(AuthAccounts authAccounts) {
+    return new CreateSignupResponse(
+        authAccounts.getId(),
+        authAccounts.getUsername(),
+        authAccounts.getAccountStatus()
+    );
+  }
+
+}
